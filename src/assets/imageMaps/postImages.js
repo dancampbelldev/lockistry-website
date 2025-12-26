5c236c15805288f207fe0690c2ddcc8337545bba
TheGrufus <157811306+TheGrufus@users.noreply.github.com>
2025-12-26 18:01:04 +0000

    Added a new article via json and image map

--- src/assets/imageMaps/postImages.js
+++ src/assets/imageMaps/postImages.js
@@ -2,16 +2,19 @@
 import abusThumb from "../images/post-images/thumbnails/abus-granit-xplus.jpg";
 import smartLockThumb from "../images/post-images/thumbnails/smart-lock-claims.jpg";
 import falseSetThumb from "../images/post-images/thumbnails/false-set.jpg";
+import learningCurveThumb from "../images/post-images/thumbnails/lock-picking-learning-curve.jpg";
 
 // Hero images
 import abusHero from "../images/post-images/hero/abus-granit-xplus.jpg";
 import smartLockHero from "../images/post-images/hero/smart-lock-claims.jpg";
 import falseSetHero from "../images/post-images/hero/false-set.jpg";
+import learningCurveHero from "../images/post-images/hero/lock-picking-learning-curve.jpg";
 
 // Inline images
 import abusInline from "../images/post-images/inline/abus-granit-xplus.jpg";
 import smartLockInline from "../images/post-images/inline/smart-lock-claims.jpg";
 import falseSetInline from "../images/post-images/inline/false-set.jpg";
+import learningCurveInline from "../images/post-images/inline/lock-picking-learning-curve.jpg";
 
 export const postImages = {
   "abus-granit-xplus": {
@@ -28,5 +31,10 @@ export const postImages = {
     thumbnail: falseSetThumb,
     hero: falseSetHero,
     inline: falseSetInline
+  },
+  "lock-picking-learning-curve": {
+    thumbnail: learningCurveThumb,
+    hero: learningCurveHero,
+    inline: learningCurveInline
   }
 };
