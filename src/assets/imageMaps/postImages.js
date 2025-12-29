643ac4300dba7797e3c192451bfba3ef0f481ada
TheGrufus <157811306+TheGrufus@users.noreply.github.com>
2025-12-29 12:29:15 +0000

    Added new article and images

--- src/assets/imageMaps/postImages.js
+++ src/assets/imageMaps/postImages.js
@@ -3,18 +3,21 @@ import abusThumb from "../images/post-images/thumbnails/abus-granit-xplus.jpg";
 import smartLockThumb from "../images/post-images/thumbnails/smart-lock-claims.jpg";
 import falseSetThumb from "../images/post-images/thumbnails/false-set.jpg";
 import learningCurveThumb from "../images/post-images/thumbnails/lock-picking-learning-curve.jpg";
+import locksportEthicsThumb from "../images/post-images/thumbnails/locksport-ethics.jpg";
 
 // Hero images
 import abusHero from "../images/post-images/hero/abus-granit-xplus.jpg";
 import smartLockHero from "../images/post-images/hero/smart-lock-claims.jpg";
 import falseSetHero from "../images/post-images/hero/false-set.jpg";
 import learningCurveHero from "../images/post-images/hero/lock-picking-learning-curve.jpg";
+import locksportEthicsHero from "../images/post-images/hero/locksport-ethics.jpg";
 
 // Inline images
 import abusInline from "../images/post-images/inline/abus-granit-xplus.jpg";
 import smartLockInline from "../images/post-images/inline/smart-lock-claims.jpg";
 import falseSetInline from "../images/post-images/inline/false-set.jpg";
 import learningCurveInline from "../images/post-images/inline/lock-picking-learning-curve.jpg";
+import locksportEthicsInline from "../images/post-images/inline/locksport-ethics.jpg";
 
 export const postImages = {
   "abus-granit-xplus": {
@@ -36,5 +39,10 @@ export const postImages = {
     thumbnail: learningCurveThumb,
     hero: learningCurveHero,
     inline: learningCurveInline
+  },
+  "locksport-ethics": {
+    thumbnail: locksportEthicsThumb,
+    hero: locksportEthicsHero,
+    inline: locksportEthicsInline
   }
 };
